3a000612b22e16f65fe4f04912b2fb554918e9fe
mmartofel <mmartofe@redhat.com>
2025-10-24 00:49:05 +0200

    add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# Following script sets up the necessary resources for the application
+
+oc apply -k ./namespace
+oc apply -k ./llama-server
+oc apply -k ./redis
+oc apply -k ./postgres
+oc apply -k ./n8n
